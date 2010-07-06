6699cec4803e6ee4bf2dfb824ec8e405ff16d2ef
Lloyd Pick <lloydpick@gmail.com>
2010-07-06 03:54:38 +0100

    adjusted defaults for github settings

--- lib/generators/devise/oauth2_authenticatable/oauth2_authenticatable_generator.rb
+++ lib/generators/devise/oauth2_authenticatable/oauth2_authenticatable_generator.rb
@@ -3,10 +3,10 @@ module Devise
     source_root File.expand_path('../templates', __FILE__)
     argument :client_id, :type => :string, :default => "YOUR_APP_API_ID"
     argument :client_key, :type => :string, :default => "YOUR_APP_SECRET_KEY"
-    argument :requested_scope, :type => :string, :default => "email,offline_access,publish_stream"
-    argument :auth_server, :type => :string, :default => "https://graph.facebook.com"
-    argument :authorize_path, :type => :string, :default => "/oauth/authorize"
-    argument :access_token_path, :type => :string, :default => "/oauth/access_token"
+    argument :requested_scope, :type => :string, :default => "user,public_repo,repo,gist"
+    argument :auth_server, :type => :string, :default => "https://github.com"
+    argument :authorize_path, :type => :string, :default => "/login/oauth/authorize"
+    argument :access_token_path, :type => :string, :default => "/login/oauth/access_token"
     desc "Generates a OAuth2 config file for your application. All the parameters are optional."
     
     def generate_oauth2_config
